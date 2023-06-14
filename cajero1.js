2db798ada992812c069f0371126241ee083679d2
pelota68 <jossie.david8@gmail.com>
2023-06-13 20:33:21 -0500

    cajero

---
+++ cajero1.js
@@ -0,0 +1,44 @@
+ /*
+ const cuenta1="mali";
+let monto = 200;
+ const montoretirar=0;
+let nuevoSaldo= monto-montoretirar;
+
+
+
+function nuevoSaldo(monto,montoretirar)
+{
+
+    let nuevoSaldo = monto-montoretirar
+    return "nuevo saldo es:"+nuevoSaldo;
+}
+
+nuevoSaldo(200,150)
+alert(" aplicacion cajero");
+
+prompt("ingrese su nombre");
+prompt("consulte su saldo");
+prompt("ingrese el monto a retirar");
+prompt("nuevo saldo");
+
+/*console.log("el nuevo saldo es"+ nuevoSaldo                  
+                    
+/*
+return sal
+console.log("su saldo es ",sal)*/
+let nombre="mali";
+const saldo = "200";
+alert("bienvenido"+nombre+ "tu saldo es "+saldo);
+
+function dinero (saldo){
+     let dinero;
+     prompt("ingrese dinero"+dinero);
+    newsaldo=saldo+dinero
+    if(saldo<=1000)
+    alert("el valor consignado fue"+dinero+"su nuevo saldo es"+newsaldo);
+}
+
+
+
+
+
